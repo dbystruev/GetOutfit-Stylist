7a249e8aa22108eac01667e044aa5bd4576bdcd4
Denis Bystruev <github@learnSwift.ru>
2020-02-04 23:11:09 +0300

    Implemented Firebase CRUD

--- lib/pages/timeline.dart
+++ lib/pages/timeline.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:getoutfit_stylist/widgets/header.dart';
@@ -11,6 +12,8 @@ class Timeline extends StatefulWidget {
 }
 
 class _TimelineState extends State<Timeline> {
+  final Random random = Random();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,8 +33,7 @@ class _TimelineState extends State<Timeline> {
               .toList();
 
           // Remove entries with empty username
-          usernames
-              .removeWhere((username) => username.isEmpty);
+          usernames.removeWhere((username) => username.isEmpty);
 
           final List<Text> children =
               usernames.map((username) => Text(username)).toList();
@@ -41,11 +43,97 @@ class _TimelineState extends State<Timeline> {
               child: ListView(
                 children: children,
               ),
+              padding: EdgeInsets.symmetric(
+                horizontal: 50,
+                vertical: 10,
+              ),
             ),
           );
         },
         stream: usersRef.snapshots(),
       ),
     );
+  }
+
+  createUser(String username) async {
+    return await usersRef.add({
+      'username': username,
+      'postsCount': 0,
+      'isAdmin': false,
+    }).catchError((error) {
+      print('ERROR creating user $username: $error');
+      return null;
+    });
+  }
+
+  deleteLastUser(String username) async {
+    final QuerySnapshot snapshot =
+        await usersRef.where('username', isEqualTo: username).getDocuments();
+    return snapshot.documents.isEmpty
+        ? null
+        : deleteUser(snapshot.documents.last.documentID);
+  }
+
+  deleteUser(String id) async {
+    final doc = await usersRef.document(id).get().catchError((error) {
+      print('ERROR deleting user $id: $error');
+      return null;
+    });
+    return doc.exists ? await doc.reference.delete() : null;
+  }
+
+  String randomUsername() {
+    const String consonants = 'bcdfghjklmnpqrstvwxz';
+    const String vowels = 'aeiouy';
+    final int length = random.nextInt(6) + 3;
+    String username = '';
+    for (int count = 0; count < length; count++) {
+      int index = random.nextInt(consonants.length * vowels.length);
+      username += count.isEven
+          ? consonants[index % consonants.length]
+          : vowels[index % vowels.length];
+    }
+    return username;
+  }
+
+  updateFirstUser(String username,
+      {String newUsername, bool isAdmin, int postsCount}) async {
+    final QuerySnapshot snapshot =
+        await usersRef.where('username', isEqualTo: username).getDocuments();
+    return snapshot.documents.isEmpty
+        ? null
+        : updateUser(
+            snapshot.documents.last.documentID,
+            newUsername: newUsername,
+            isAdmin: isAdmin,
+            postsCount: postsCount,
+          );
+  }
+
+  updateUser(String id,
+      {String newUsername, bool isAdmin, int postsCount}) async {
+    final doc = await usersRef.document(id).get().catchError((error) {
+      print('ERROR updating user $id: $error');
+      return null;
+    });
+    return doc.exists
+        ? await doc.reference.updateData({
+            'username': newUsername ?? doc['username'],
+            'isAdmin': isAdmin ?? doc['isAdmin'],
+            'postsCount': postsCount ?? doc['postsCount'],
+          })
+        : null;
+  }
+
+  @override
+  void initState() {
+    final String username = randomUsername();
+    super.initState();
+    createUser(username);
+    if (random.nextBool()) {
+      updateFirstUser(username, newUsername: randomUsername());
+    } else {
+      deleteLastUser(username);
+    }
   }
 }
