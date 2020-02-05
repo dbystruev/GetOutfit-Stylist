094f80432fe6a3a7a964f9ec36f111e4c7fd8f3e
Denis Bystruev <github@learnSwift.ru>
2020-02-05 23:36:37 +0300

    Search screen UI

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class Search extends StatefulWidget {
   @override
@@ -6,10 +7,61 @@ class Search extends StatefulWidget {
 }
 
 class _SearchState extends State<Search> {
+  AppBar buildSearchField() {
+    return AppBar(
+      backgroundColor: Colors.white,
+      title: TextFormField(
+        decoration: InputDecoration(
+          filled: true,
+          hintText: "Search for a user...",
+          prefixIcon: Icon(
+            Icons.account_box,
+            size: 28,
+          ),
+          suffixIcon: IconButton(
+            icon: Icon(Icons.clear),
+            onPressed: () => print('cleared'),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Container buildNoContent() {
+    return Container(
+      child: Center(
+        child: ListView(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(left: 70),
+              child: SvgPicture.asset(
+                'assets/images/search.svg',
+                height: 300,
+              ),
+            ),
+            Text(
+              'Find Users',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 60,
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.w600,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ],
+          shrinkWrap: true,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Search'),
+    return Scaffold(
+      appBar: buildSearchField(),
+      backgroundColor: Theme.of(context).primaryColor.withOpacity(0.8),
+      body: buildNoContent(),
     );
   }
 }
@@ -17,6 +69,8 @@ class _SearchState extends State<Search> {
 class UserResult extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text('User Result');
+    return Center(
+      child: Text('UserResult'),
+    );
   }
 }
