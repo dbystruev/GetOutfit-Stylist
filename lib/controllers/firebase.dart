9d165ddbea5f0c02e0263d93eaf1ebb1d06ad119
Denis Bystruev <dbystruev@me.com>
2020-02-17 00:10:20 +0300

    Comments logic

--- lib/controllers/firebase.dart
+++ lib/controllers/firebase.dart
@@ -2,10 +2,12 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
-final CollectionReference looksRef = Firestore.instance.collection('looks');
-final CollectionReference usersRef = Firestore.instance.collection('users');
+final CollectionReference commentsRef =
+    Firestore.instance.collection('comments');
 final GoogleSignIn googleSignIn = GoogleSignIn();
+final CollectionReference looksRef = Firestore.instance.collection('looks');
 final StorageReference storageRef = FirebaseStorage.instance.ref();
+final CollectionReference usersRef = Firestore.instance.collection('users');
 
 void login() {
   googleSignIn.signIn();
--- lib/pages/comments.dart
+++ lib/pages/comments.dart
@@ -1,5 +1,11 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:getoutfit_stylist/controllers/firebase.dart';
+import 'package:getoutfit_stylist/pages/home.dart';
 import 'package:getoutfit_stylist/widgets/header.dart';
+import 'package:getoutfit_stylist/widgets/progress.dart';
+import 'package:timeago/timeago.dart' as timeago;
 
 class Comments extends StatefulWidget {
   final String lookId;
@@ -25,12 +31,24 @@ class _CommentsState extends State<Comments> {
   final String lookId;
   final String lookOwnerId;
   final String lookMediaUrl;
+  bool noComment = true;
 
   _CommentsState({
     this.lookId,
     this.lookOwnerId,
     this.lookMediaUrl,
   });
+
+  void addComment() {
+    commentsRef.document(lookId).collection('comments').add({
+      'avatarUrl': currentUser.photoUrl,
+      'comment': commentController.text.trim(),
+      'timestamp': timestamp,
+      'userId': currentUser.id,
+      'username': currentUser.username,
+    });
+    commentController.clear();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -49,15 +67,20 @@ class _CommentsState extends State<Comments> {
                   controller: commentController,
                   decoration: InputDecoration(labelText: 'Write a comment...'),
                 ),
-                trailing: OutlineButton(
-                  borderSide: BorderSide.none,
-                  child: Icon(
-                    Icons.send,
-                    color: Theme.of(context).accentColor,
-                    size: 28,
-                  ),
-                  onPressed: () => print('Add a comment'),
-                ),
+                trailing: noComment
+                    ? Text('')
+                    : OutlineButton(
+                        borderSide: BorderSide.none,
+                        child: Icon(
+                          Icons.send,
+                          color: Theme.of(context).accentColor,
+                          size: 28,
+                        ),
+                        onPressed: () {
+                          if (commentController.text.trim().isNotEmpty)
+                            addComment();
+                        },
+                      ),
               ),
             ],
           ),
@@ -67,15 +90,78 @@ class _CommentsState extends State<Comments> {
   }
 
   Widget buildComments() {
-    return Text('Comment');
+    return StreamBuilder(
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) return circularProgress(context);
+        List<Comment> comments = [];
+        snapshot.data.documents.forEach((doc) {
+          comments.add(
+            Comment.fromDocument(doc),
+          );
+        });
+        return ListView(children: comments);
+      },
+      stream: commentsRef
+          .document(lookId)
+          .collection('comments')
+          .orderBy('timestamp', descending: true)
+          .snapshots(),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    commentController.addListener(() {
+      setState(() {
+        noComment = commentController.text.trim().isEmpty;
+      });
+    });
   }
 }
 
 class Comment extends StatelessWidget {
+  final String avatarUrl;
+  final String comment;
+  final Timestamp timestamp;
+  final String userId;
+  final String username;
+
+  Comment({
+    this.avatarUrl,
+    this.comment,
+    this.timestamp,
+    this.userId,
+    this.username,
+  });
+
+  factory Comment.fromDocument(DocumentSnapshot doc) {
+    return Comment(
+      avatarUrl: doc['avatarUrl'],
+      comment: doc['comment'],
+      timestamp: doc['timestamp'],
+      userId: doc['userId'],
+      username: doc['username'],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('Comment'),
+      child: Column(children: <Widget>[
+        ListTile(
+          leading: CircleAvatar(
+            backgroundImage: CachedNetworkImageProvider(avatarUrl),
+          ),
+          subtitle: Text(
+            timeago.format(
+              timestamp.toDate(),
+            ),
+          ),
+          title: Text(comment),
+        ),
+        Divider(),
+      ]),
     );
   }
 }
